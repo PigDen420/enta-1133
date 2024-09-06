ffe2be262c4375282ddcea90b6cdcc051d0008e2
_PigDen_ <jayguzman4200@gmail.com>
2024-09-06 12:14:19 -0700

    Exploring Visual Studio
    Basic code with an extra line.

---
+++ GD12_1133_Lab1_DeGuzman_Jayden/GD12_1133_Lab1_DeGuzman_Jayden/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GD12_1133_Lab1_DeGuzman_Jayden /*(Comment/Notes); namespace = file name*/
+{ /*This is the top bun of the burger*/
+    internal class Program
+    { 
+        static void Main(string[] args) /*static void = Start of the executable program*/
+        {  /*These are the toppings*/
+            Console.WriteLine("What's good y'all?"); /*WriteLine = Display sentence; Every group of code start and end with{}; Every line of code ends with ";"*/ /*Red circle = Breakpoint & Enter Debugging mode*/
+            Console.WriteLine("I said, what's Good y'all?");
+        }
+    }
+} /*This is the bottom bun of the burger*/
